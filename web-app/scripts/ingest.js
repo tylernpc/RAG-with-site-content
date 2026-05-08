873230d0c44045a7cfeedebd559bba710c349396
tylernpc <tylerlegm@gmail.com>
2026-05-07 21:28:10 -0500

    ingest.js

---
+++ web-app/scripts/ingest.js
@@ -0,0 +1,17 @@
+import "cheerio";
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+
+const pTagSelector = "p";
+const cheerioLoader = new CheerioWebBaseLoader(
+  "https://lilianweng.github.io/posts/2023-06-23-agent/",
+  {
+    selector: pTagSelector,
+  },
+);
+
+const docs = await cheerioLoader.load();
+
+console.assert(docs.length === 1);
+console.log(`Total characters: ${docs[0].pageContent.length}`);
+
+// node ingest.js to run
